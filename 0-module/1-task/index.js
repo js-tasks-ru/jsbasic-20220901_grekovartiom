015cf06f7e154865e6af672ba164c5b4b064ad78
Artem Grekov <grekovartiom@gmail.com>
2022-09-04 01:10:26 +0500

    Сделал суммирование двух чисел

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,5 @@
-function sum(m, n) {
-  // ваш код...
-}
+let x = prompt("Введите первое число",'');
+let y = prompt("Введите второе число",'');
+var value1 = parseInt(x);
+var value2 = parseInt(y);
+alert(value1+value2);
