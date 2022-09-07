aa8ccb794f73a37a62823b7deeb9e54e6ad5963f
Artem Grekov <grekovartiom@gmail.com>
2022-09-08 00:01:15 +0500

    summirovanie dvuh chisel

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,5 +1,3 @@
-let x = prompt("Введите первое число",'');
-let y = prompt("Введите второе число",'');
-var value1 = parseInt(x);
-var value2 = parseInt(y);
-alert(value1+value2);
+function sum(m, n) {
+    return m + n;
+  }
