468d87ba98c0bca9f4b1e60e30e513c015442d3c
Artem Grekov <grekovartiom@gmail.com>
2022-09-08 19:48:08 +0500

    problemi v 1 i 5 zadache

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
-  // ваш код...
+  if (str.includes ("1XbeT now") ) {
+    return true;
+  } else if
+  (str.includes ("free xxxxx") ) {
+    return true;
+  } else if
+  (str.includes ("innocent rabbit") ) {
+    return false;
+  }
+    
 }
