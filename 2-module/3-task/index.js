debda08c6b0c4742911a2c8b7a91138857515237
Artem Grekov <grekovartiom@gmail.com>
2022-09-13 01:00:23 +0500

    reshenie

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
-};
+  mul : function () {
+    return this.a * this.b
+  } ,
+  sum : function () {
+    return this.a + this.b
+  },
+  read : function (a,b) {
+    this.a = a;
+    this.b = b;
+  } ,
+
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
